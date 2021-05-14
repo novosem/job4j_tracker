a0738d140fa5db500925de7ab81ace098438d54f
Sam <novosem@mail.ru>
2021-05-14 21:20:53 +0500

    9. toString [#521293]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -10,8 +10,10 @@ public class StartUI {
         tracker.add(new Item(1, "Ivan"));
         tracker.add(new Item(2, "Petya"));
         tracker.add(new Item(4, "Vasya"));
+        Item alex = new Item("Alex");
         System.out.println(tracker.findById(4));
         System.out.println(Arrays.toString(tracker.findAll()));
         System.out.println(Arrays.toString(tracker.findByName("Ivan")));
+        System.out.println(tracker.replace(2, alex));
     }
 }
