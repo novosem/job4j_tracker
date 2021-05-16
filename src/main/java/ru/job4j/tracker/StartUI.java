aa14a54ce9fdf00998466776fbc17a302de94845
дизель-сервис <novosem@mail.ru>
2021-05-16 13:01:48 +0500

    2.2. Реализация класса StartUI. Вывод меню [#521219]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -9,78 +9,28 @@ public class StartUI {
     public void init(Scanner scanner, Tracker tracker) {
         boolean run = true;
         while (run) {
-            this.showMenu();
+            showMenu();
             System.out.print("Select: ");
-            int select = Integer.valueOf(scanner.nextLine());
-            if (select == 0) {
-                System.out.println("=== Create a new Item ====");
-                System.out.print("Enter name: ");
-                String name = scanner.nextLine();
-                Item item = new Item(name);
-                tracker.add(item);
-                } else if (select == 1) {
-                Item[] allItem = tracker.findAll();
-                for (int i = 0; i < allItem.length; i++) {
-                    System.out.println(allItem[i]);
-                }
-            } else if (select == 2) {
-                System.out.println("Enter Id to edit:");
-                int id = Integer.parseInt(scanner.nextLine());
-                System.out.println("Enter new Name:");
-                String name = scanner.nextLine();
-                Item newItem = new Item(name);
-                tracker.replace(id, newItem);
-                if (tracker.replace(id, newItem)) {
-                    System.out.println("Edit complete");
-                } else {
-                    System.out.println("No id found");
-                }
-            } else if (select == 3) {
-                System.out.println("Enter Id:");
-                int id = Integer.parseInt(scanner.nextLine());
-                tracker.delete(id);
-                if (tracker.delete(id)) {
-                    System.out.println("No id found");
-                } else {
-                    System.out.println("Id = " + id + " deleted");
-                }
-            }else  if (select == 4) {
-                System.out.println("Enter Id:");
-                int id = Integer.parseInt(scanner.nextLine());
-                Item item = tracker.findById(id);
-                if (item != null) {
-                    System.out.println(item);
-                } else {
-                    System.out.println("No id found");
-                }
-            } else  if (select == 5) {
-                System.out.println("Enter name:");
-                String name = scanner.nextLine();
-                Item[] item = tracker.findByName(name);
-                if (item.length > 0) {
-                    for (int i = 0; i < item.length; i++) {
-                        System.out.println(item[i]);
-                    }
-                } else {
-                    System.out.println("Entered name not found");
-                }
-            } else if (select == 6) {
+            int select = Integer.parseInt(scanner.nextLine());
+            if (select != 6) {
+                System.out.println("Пользователь выбрал: " + select);
+            } else {
                 run = false;
             }
         }
     }
 
     private void showMenu() {
-        System.out.println("Menu.");
-        System.out.println("0. Add new items");
-        System.out.println("1. Show all items");
-        System.out.println("2. Edit item");
-        System.out.println("3. Delete item");
-        System.out.println("4. Find item by id");
-        System.out.println("5. Find items by name");
-        System.out.println("6. Exit Program");
+        String[] menu = {
+                "Add new Item", "Show all items", "Edit item",
+                "Delete item", "Find item by id", "Find items by name",
+                "Exit Program"
+        };
+        System.out.println("Menu:");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + ". " + menu[i]);
+        }
     }
-
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
