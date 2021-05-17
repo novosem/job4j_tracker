3acc0ed699641033eb5e8c64d55c4be0d59982b6
дизель-сервис <novosem@mail.ru>
2021-05-17 17:14:09 +0500

    3. Интерфейс Input [#521210]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,17 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport{
+
+    @Override
+    public void drive() {
+    }
+
+    @Override
+    public void passengers(int quantityOfPass) {
+    }
+
+    @Override
+    public float refuel(int petrol) {
+        return 0;
+    }
+}
---
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -0,0 +1,10 @@
+package ru.job4j.poly;
+
+public interface Transport {
+
+    void  drive ();
+
+    void passengers (int quantityOfPass);
+
+    float refuel (int petrol);
+}
