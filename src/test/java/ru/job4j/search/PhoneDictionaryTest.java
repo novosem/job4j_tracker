56edf8f3c8b6716ff4a27412fea6326b058ee5bb
дизель-сервис <novosem@mail.ru>
2021-05-30 20:16:34 +0500

    1. Телефонный справочник на базе ArrayList [#41598 #162202]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -18,4 +18,14 @@ public class PhoneDictionaryTest {
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
     }
+
+    @Test
+    public void whenFindNothing() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Ivan");
+        assertThat(persons.size(), is(0));
+    }
 }
