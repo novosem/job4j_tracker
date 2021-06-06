24646951e81c52b27f59d8f2054cf884c3d5fdc2
дизель-сервис <novosem@mail.ru>
2021-06-06 15:48:08 +0500

    2. Комбинированный компаратор [#10036]

--- src/main/java/ru/job4j/collection/Job.java
+++ src/main/java/ru/job4j/collection/Job.java
@@ -1,5 +1,7 @@
 package ru.job4j.collection;
 
+import java.util.Objects;
+
 public class Job implements Comparable<Job> {
     private String name;
 
@@ -29,5 +31,19 @@ public class Job implements Comparable<Job> {
     @Override
     public int compareTo(Job another) {
         return Integer.compare(priority, another.priority);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Job job = (Job) o;
+        return priority == job.priority &&
+                Objects.equals(name, job.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, priority);
     }
 }
---
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -0,0 +1,85 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class JobTest {
+    @Test
+    public void whenCompatorByNameAndPrority() {
+        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+
+    @Test
+    public void whenComparatorByName() {
+        List<Job> job = Arrays.asList(new Job("Impl task", 0),
+        new Job("Fix bug", 1));
+        Collections.sort(job, new SortByNameJob());
+        List<Job> expect = Arrays.asList(
+                new Job("Fix bug", 1),
+                new Job("Impl task", 0)
+        );
+        assertThat(job, is(expect));
+    }
+
+    @Test
+    public void whenComparatorByPriority() {
+        List<Job> job = Arrays.asList(new Job("Impl task", 0),
+                new Job("Fix bug", 1));
+        Collections.sort(job, new JobDescByPriority());
+        List<Job> expect = Arrays.asList(
+                new Job("Fix bug", 1),
+                new Job("Impl task", 0)
+        );
+        assertThat(job, is(expect));
+    }
+
+    @Test
+    public void whenComparatorDescByName() {
+        List<Job> job = Arrays.asList(new Job("Impl task", 0),
+                new Job("Fix bug", 1));
+        Collections.sort(job, new JobDescByName());
+        List<Job> expect = Arrays.asList(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+
+        );
+        assertThat(job, is(expect));
+    }
+
+    @Test
+    public void whenComparatorDescByPriority() {
+        List<Job> job = Arrays.asList(new Job("Impl task", 0),
+                new Job("Fix bug", 1));
+        Collections.sort(job);
+        List<Job> expect = Arrays.asList(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+
+        );
+        assertThat(job, is(expect));
+    }
+
+    @Test
+    public void whenCompatorSortByNameAndPrority() {
+        Comparator<Job> cmpNamePriority = new SortByNameJob().thenComparing(new JobDescByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+}
