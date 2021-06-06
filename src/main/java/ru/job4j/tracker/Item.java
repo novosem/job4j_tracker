34bb422b204f7e4b3dde5dfce2b5dc755a5c1c29
дизель-сервис <novosem@mail.ru>
2021-06-06 14:06:29 +0500

    0. Сортировка [#10096]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,11 @@
 package ru.job4j.tracker;
 
-import java.time.LocalDateTime;
+import ru.job4j.collection.Order;
 
-public class Item {
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Item implements Comparable<Item>{
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
@@ -46,4 +49,22 @@ public class Item {
                 ", name='" + name + '\'' + "}";
     }
 
+    @Override
+    public int compareTo(Item another) {
+        return Integer.compare(id, another.id);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return Objects.equals(id, item.id) &&
+                Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
---
+++ src/test/java/ru/job4j/tracker/ItemTest.java
@@ -0,0 +1,46 @@
+package ru.job4j.tracker;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ItemTest {
+
+    @Test
+    public void compareTo() {
+        List<Item> items = Arrays.asList(
+                new Item(2, "QQQ"),
+                new Item(3, "WWW"),
+                new Item(1,"TTT")
+        );
+        Collections.sort(items);
+        List<Item> item = Arrays.asList(
+                new Item(1,"TTT"),
+                new Item(2, "QQQ"),
+                new Item(3, "WWW")
+        );
+        assertThat(items, is(item));
+    }
+
+    @Test
+    public void compareToBack() {
+        List<Item> items = Arrays.asList(
+                new Item(2, "QQQ"),
+                new Item(3, "WWW"),
+                new Item(1,"TTT")
+        );
+        items.sort(Collections.reverseOrder());
+        List<Item> item = Arrays.asList(
+                new Item(3, "WWW"),
+                new Item(2, "QQQ"),
+                new Item(1,"TTT")
+        );
+        assertThat(items, is(item));
+    }
+}
