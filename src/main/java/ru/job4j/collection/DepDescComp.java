630d4250c9075d336a235f8c0a1450141eae9c12
дизель-сервис <novosem@mail.ru>
2021-06-15 14:36:21 +0500

    2. Отсортировать департаменты [#34352 #167849]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -7,8 +7,9 @@ public class DepDescComp implements Comparator<String> {
     public int compare(String o1, String o2) {
         String[] a1 = o1.split("/");
         String[] a2 = o2.split("/");
-        if (a1.length - a2.length == 0) {
-            return o1.compareTo(o2);
+        int rsl = a1[0].compareTo(a2[0]);
+        if (rsl != 0) {
+            return rsl;
         }
         return a1[a1.length - 1].compareTo(a2[a2.length - 1]);
     }
