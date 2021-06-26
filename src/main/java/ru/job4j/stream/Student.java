935dc7d9b9fe185a0966f2b9181e8f4bb1737aef
дизель-сервис <novosem@mail.ru>
2021-06-26 21:05:26 +0500

    3. Уникальность элементов и сортировка. [#110225]

--- src/main/java/ru/job4j/stream/Student.java
+++ src/main/java/ru/job4j/stream/Student.java
@@ -37,4 +37,9 @@ public class Student {
     public int hashCode() {
         return Objects.hash(score, surname);
     }
+
+    @Override
+    public String toString() {
+        return "Student{score=" + score + ", surname='" + surname + '\'' + '}';
+    }
 }
