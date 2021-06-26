472fea9e5aec1ef2ad958a0ced2671bad3d1035c
дизель-сервис <novosem@mail.ru>
2021-06-26 12:11:13 +0500

    0. Stream API [#58166]

---
+++ src/main/java/ru/job4j/stream/StreamUsage.java
@@ -0,0 +1,45 @@
+package ru.job4j.stream;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamUsage {
+    public static class Task {
+        private final String name;
+        private final long spent;
+
+        public Task(String name, long spent) {
+            this.name = name;
+            this.spent = spent;
+        }
+
+        @Override
+        public String toString() {
+            return "Task{" + "name='" + name + '\''
+                    + ", spent=" + spent + '}';
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Task> tasks = List.of(
+                new Task("Bug #1", 100),
+                new Task("Task #2", 100),
+                new Task("Bug #3", 100)
+        );
+        List<Task> bugs = tasks.stream().filter(
+                task -> task.name.contains("Bug")
+        ).collect(Collectors.toList());
+        bugs.forEach(System.out::println);
+        List<Integer> integers = new ArrayList<>();
+        integers.add(1);
+        integers.add(2);
+        integers.add(-3);
+        integers.add(4);
+        integers.add(5);
+        integers.add(-1);
+        List<Integer> integers1 = integers.stream().filter(integer ->
+                integer >= 0).collect(Collectors.toList());
+        integers1.forEach(System.out :: println);
+    }
+}
